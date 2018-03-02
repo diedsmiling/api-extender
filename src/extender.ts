0cd8cc166c9e90de57409a7a3781ec467debe76f
lazarev <diedsmiling@gmail.com>
2018-03-02 23:09:52 +0100

    Added delete method

--- src/extender.ts
+++ src/extender.ts
@@ -1,8 +1,11 @@
-import { update } from 'lodash';
+import { update, unset } from 'lodash';
 import { ExtenderInterface, jsonObject } from './types';
 
 export class Extender implements ExtenderInterface {
   update(path: string, newValue: jsonObject) {
     return (body: jsonObject) => update(body, path, () => newValue);
+  }
+  delete(path: string) {
+    return (body: jsonObject) => { unset(body, path); };
   }
 }
