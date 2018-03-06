d59ab0f9770f4e9604a6a006f82706e9d428d463
lazarev <diedsmiling@gmail.com>
2018-03-06 07:14:56 +0100

    Added more tests

--- src/__tests__/middleware.test.ts
+++ src/__tests__/middleware.test.ts
@@ -28,8 +28,9 @@ test('Should set new value to nested path', async (t) => {
       bar: 'baz'
     }
   };
-  const app = createServer(extender.set('foo.bar', 'New value'), defaultBody)
+  const app = createServer(extender.set('foo.bar', 'New value'), defaultBody);
   const { body } = await request(app).get('/foo');
+
   t.deepEqual(body, { foo: { bar: 'New value' } });
 });
 
@@ -39,8 +40,75 @@ test('Should set new object value to nested path', async (t) => {
       bar: 'baz'
     }
   };
-  const app = createServer(extender.set('foo.bar', { baz: 'quax' }), defaultBody)
+  const app = createServer(extender.set('foo.bar', { baz: 'qux' }), defaultBody);
   const { body } = await request(app).get('/foo');
-  t.deepEqual(body, { foo: { bar: { baz: 'quax' } } });
+
+  t.deepEqual(body, { foo: { bar: { baz: 'qux' } } });
+});
+
+test('Should add new value to object', async (t) => {
+  const defaultBody = {
+    foo: {
+      bar: 'baz'
+    }
+  };
+  const app = createServer(extender.set('foo.baz', 'qux'), defaultBody);
+  const { body } = await request(app).get('/foo');
+
+  t.deepEqual(body, { foo: { bar: 'baz', baz: 'qux' } });
+});
+
+
+test('Should replace element in array', async (t) => {
+  const defaultBody = {
+    foo: {
+      bar: [0, 1, 3, 3]
+    }
+  };
+
+  const app = createServer(extender.set('foo.bar.2', 2), defaultBody);
+  const { body } = await request(app).get('/foo');
+
+  t.deepEqual(body, { foo: { bar: [0, 1, 2, 3] } });
+});
+
+test('Should delete a property', async (t) => {
+  const defaultBody = {
+    foo: {
+      bar: 'baz',
+      baz: 'qux'
+    }
+  };
+
+  const app = createServer(extender.delete('foo.bar'), defaultBody);
+  const { body } = await request(app).get('/foo');
+
+  t.deepEqual(body, { foo: { baz: 'qux' } });
+});
+
+test('Should push element into array', async (t) => {
+  const defaultBody = {
+    foo: {
+      bar: [0, 1, 2, 3]
+    }
+  };
+
+  const app = createServer(extender.push('foo.bar', 4), defaultBody);
+  const { body } = await request(app).get('/foo');
+
+  t.deepEqual(body, { foo: { bar: [0, 1, 2, 3, 4] } });
+});
+
+test('Should unshift element into array', async (t) => {
+  const defaultBody = {
+    foo: {
+      bar: [1, 2, 3]
+    }
+  };
+
+  const app = createServer(extender.unshift('foo.bar', 0), defaultBody);
+  const { body } = await request(app).get('/foo');
+
+  t.deepEqual(body, { foo: { bar: [0, 1, 2, 3] } });
 });
 
